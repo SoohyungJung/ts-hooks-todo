b067bf5ace1f4bdf618b5dded305dac372d80551
SOOHYUNG JUNG <sjung0903@gmail.com>
2020-04-23 21:16:31 +0900

    Added complete button

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import React, { useState } from 'react';
 type FormElem = React.FormEvent<HTMLFormElement>
 
 interface ITodo {
-  text: string
+  text: string,
+  complete: boolean
 }
 
 const App: React.FC = () => {
@@ -17,25 +18,38 @@ const App: React.FC = () => {
   }
 
   const handleTodoAdd = (text: string): void => {
-    const newTodos: ITodo[] = [...todos, { text }];
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  }
+
+  const handleComplete = (i: number): void => {
+    const newTodos = [...todos];
+    newTodos[i].complete = !newTodos[i].complete;
     setTodos(newTodos);
   }
 
   return (
     <div>
       <h1>Todo List</h1>
-      <form onClick={handleSubmit}>
-        <input type='text' value={value} onChange={e => setValue(e.target.value)} />
-        <button type='submit'>Add Todo</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button type="submit">Add Todo</button>
       </form>
 
       <section>
         {todos.map((item: ITodo, i: number) => (
-          <div key={i}>{item.text}</div>
+          <div key={i.toString()}>
+            <div>{item.text}</div>
+            <button type="button" onClick={() => handleComplete(i)}>{item.complete ? 'Incomplete' : 'Complete'}</button>
+          </div>
         ))}
       </section>
     </div>
-  )
+  );
 }
 
 export default App;
