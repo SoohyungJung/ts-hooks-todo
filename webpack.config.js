b13de401a41324718724ea623b623f83ef29dec6
SOOHYUNG JUNG <sjung0903@gmail.com>
2020-04-23 19:34:22 +0900

    Set webpack config & babelrc

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  target: 'web',
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: { extensions: ['.ts', '.tsx', 'js'] },
+  devServer: {
+    contentBase: './',
+    port: 5000
+  }
+}
